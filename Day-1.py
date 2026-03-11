9e1da26035266d8b6c5a34482c9e609054b45ca6
kushal060305 <kushalausula@gmail.com>
2026-03-11 12:21:06 +0530

    Initial commit: Add Day-1.py with count_even function

---
+++ Day-1.py
@@ -0,0 +1,24 @@
+'''def count_even(n):
+
+    count=0
+    for i in range(1,n+1):
+        if i%2==0:
+            count+=1
+    return count
+n=int(input("Enter a number: "))
+print("Number of even numbers from 1 to",n,"is:",count_even(n))
+'''
+
+###############################################################
+
+def count_digits(num):
+    count=0
+    while num>0:
+        num=num//10
+        count+=1    
+    return count
+
+def main():
+    num=int(input("Enter a number: "))
+    print("Number of digits in",num,"is:",count_digits(num))
+main()  
